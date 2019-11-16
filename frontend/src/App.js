42c1efa8e2546ffbd15e0602d15e01b9399a5aeb
Takuya <takuya.miraidenshi@gmail.com>
2019-11-17 05:53:40 +0900

    add: models

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo1.png';
 import './App.css';
 import Result from './components/result';
 
 function App() {
   return (
     <div>
+      <img src={logo} alt="" style={{width: '75%', textAlign: 'center'}} />
       <Result />
     </div>
   );
