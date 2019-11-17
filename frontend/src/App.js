3a4dcd8029c2d4b66f2478661dbf0b916ead9701
Takuya <takuya.miraidenshi@gmail.com>
2019-11-17 12:40:45 +0900

    fix: start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,6 @@ import React from 'react';
 import logo from './logo1.png';
 import './App.css';
 import Result from './components/result';
-import {transitions, positions, Provider as AlertProvider} from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
-
-const options = {
-  // you can also just use 'bottom center'
-  position: positions.BOTTOM_CENTER,
-  timeout: 5000,
-  offset: '30px',
-  // you can also just use 'scale'
-  transition: transitions.SCALE,
-};
 
 function App() {
   return (
@@ -25,9 +14,7 @@ function App() {
         />
       </div>
 
-      <AlertProvider template={AlertTemplate} {...options}>
-        <Result style={{textAlign: '-webkit-center'}} />
-      </AlertProvider>
+      <Result style={{textAlign: '-webkit-center'}} />
     </div>
   );
 }
